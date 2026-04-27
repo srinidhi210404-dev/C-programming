50dd6838cd556582318b517a9edd02abb4821cd8
srinidhi210404-dev <srinidhi210404@gmail.com>
2026-04-27 12:05:26 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   printf("enter the values of two numbers");
+   scanf("%d%d",&a,&b);
+   c=a+b;
+   printf("the addition of two number is %d ",c);
+   return 0;
+}
